98ca7b830632560d0f164d1c1d226f6a01aff4fb
Zfp <zfpyz2011@163.com>
2018-04-15 22:16:52 +0800

    [RENAME] launcher.__main__ -> launcher.main

--- launcher.py
+++ launcher.py
@@ -2,11 +2,11 @@ from gui.gui_panel import App
 from config_operator import read_config
 
 
-def __main__():
+def main():
     read_config()
     app = App()
     app.start()
 
 
 if __name__ == "__main__":
-    __main__()
+    main()
