94605e848816cc89ae4c06b6a1d36b10ff9d8781
Zfp <zfpyz2011@163.com>
2018-05-26 00:58:19 +0800

    update qr_util.py

--- qr_util.py
+++ qr_util.py
@@ -1,20 +1,45 @@
-# import qrcode
-# import zbarlight
-# from PIL import Image
-#
-# file_path = 'qr.png'
-#
-# opts_key_len_list = [('obfsparam=', 10), ('remark=', 7)]
-#
-# with open(file_path, 'rb') as image_file:
-#     image = Image.open(image_file)
-#     image.load()
-# codes_url = zbarlight.scan_codes('qrcode', image)
-# code = bytes.decode(codes_url[0]).lstrip("ssr://").replace("_", "/")
-# print(code)
+from pyzbar.pyzbar import decode
+import qrcode
+from PIL import Image
+from ssr_url_util import decode_ssr, encode_ssr
 
-from pyscreenshot import grab
-# import pyautogui
 
-im = grab()
-im.show()
+def decode_qr(img_path):
+    ssr_url_bytes_lst = decode(Image.open('tt.png'))
+    ssr_lst = []
+    for ssr_url_bytes in ssr_url_bytes_lst:
+        ssr = decode_ssr(ssr_url_bytes.decode("utf-8"))
+        ssr["ssr_url"] = ssr_url_bytes
+        ssr_lst.append(ssr)
+    return ssr_lst
+
+
+def encode_qr(ssr):
+    ssr_url = encode_ssr(ssr)
+    qr = qrcode.QRCode(
+        version=None,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=1,
+    )
+    qr.add_data(ssr_url)
+    qr.make(fit=True)
+    img = qr.make_image()
+    img.show()
+
+
+encode_qr({
+    "local": "127.0.0.1",
+    "local_port": "8080",
+    "method": "rc4-md5-6",
+    "obfs": "http_simple",
+    "obfsparam": "",
+    "password": "password",
+    "port": "0",
+    "protocol": "auth_aes128_sha1",
+    "protoparam": "",
+    "server": "192.168.1.1",
+    "remarks": "default"
+})
+
+decode_qr("test.png")
--- ssr_url_util.py
+++ ssr_url_util.py
@@ -2,30 +2,32 @@ import base64
 
 
 def b64_encode(content_str, altchars=b'-_'):
-    return bytes.decode(base64.b64encode(bytes(content_str), altchars))
+    return bytes.decode(base64.b64encode(str(content_str).encode('latin-1'), altchars))
 
 
 def b64_decode(content_b64):
-    return bytes.decode(base64.b64decode(content_b64), "utf-8")
+    # https://stackoverflow.com/a/9807138/5189486
+    # base64编码中末尾的'='号被省略被认为是无损的，但是decode时如果不将'='添加回去，会导致Incorrect padding error
+    missing_padding = len(content_b64) % 4
+    if missing_padding != 0:
+        content_b64 += '=' * (4 - missing_padding)
+    return bytes.decode(base64.b64decode(content_b64), 'latin-1')
 
 
 def uri_to_map(uri_str):
     uri_params = uri_str.split("&")
-    return dict(param.split("=") for param in uri_params)
+    return dict(param.split("=", maxsplit=1) for param in uri_params)
 
 
 def encode_ssr(ssr):
-    obfp_base64 = b64_encode(ssr['obfs_param'])
-    ptcp_base64 = b64_encode(ssr['protocol_param'])
-    name_base64 = b64_encode(ssr['ssr_name'])
-    locp_base64 = b64_encode(ssr['local_port'])
-    loc_base64 = b64_encode(ssr['local'])
+    obfp_base64 = b64_encode(ssr['obfsparam'])
+    ptcp_base64 = b64_encode(ssr['protoparam'])
+    name_base64 = b64_encode(ssr['remarks'])
 
     password_base64 = b64_encode(ssr['password'])
-    param_base64 = "obfsparam={}&protoparam={}&ssrname={}&localport={}&local={}".format(obfp_base64, ptcp_base64,
-                                                                                        name_base64, locp_base64,
-                                                                                        loc_base64)
-    pwd_param = b64_encode("{}/?{}".format(password_base64, param_base64))
+    param_base64 = "obfsparam={}&protoparam={}&ssrname={}".format(obfp_base64, ptcp_base64,
+                                                                  name_base64)
+    pwd_param = "{}/?{}".format(password_base64, param_base64)
 
     return "ssr://" + b64_encode("{server}:{port}:{protocol}:{method}:{obfs}:{pwd_param}"
                                  .format(server=ssr["server"], port=ssr["port"],
@@ -53,3 +55,24 @@ def decode_ssr(ssr_url_b64):
         ssr[k] = b64_decode(v)
 
     return ssr
+
+    #
+
+
+re = encode_ssr({
+    "method": "rc4-md5-6",
+    "obfs": "http_simple",
+    "obfsparam": "sdf",
+    "password": "password",
+    "port": "1253",
+    "protocol": "auth_aes128_sha1",
+    "protoparam": "",
+    "server": "192.168.1.1",
+    "remarks": "default"
+})
+print(re)
+# re_ssr = decode_ssr(
+#     "ssr://MzEuanAuaGRzb2Nrcy5jbjozMDYwNzphdXRoX2NoYWluX2E6bm9uZTp0bHMxLjJfdGlja2V0X2F1dGg6T0V4cU9YRmlPSGRETVEvP29iZnNwYXJhbT0mcHJvdG9wYXJhbT0mcmVtYXJrcz1NekV1YW5BdWFHUnpiMk5yY3k1amJnJmdyb3VwPQ")
+# print(re_ssr)
+
+print(decode_ssr(re))
